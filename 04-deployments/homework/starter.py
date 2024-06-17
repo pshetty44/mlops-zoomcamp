6c9e330a5f9b317a392e8019faaada5d28ed9496
PJ Shetty <prabhanjan.shetty@gmail.com>
2024-06-17 17:59:16 +0000

    hw chnages

--- 04-deployments/homework/starter.py
+++ 04-deployments/homework/starter.py
@@ -1,39 +1,18 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
-pip list --format=freeze | grep scikit-learn
-
-
-# In[2]:
-
-
-get_ipython().system('python -V')
-
-
-# In[9]:
-
-
 import pickle
 import pandas as pd
 import numpy as np
-
-
-# In[5]:
+import sys
 
 
 with open('model.bin', 'rb') as f_in:
     dv, model = pickle.load(f_in)
-
-
-# In[6]:
-
+2023
 
 categorical = ['PULocationID', 'DOLocationID']
 
-def read_data(filename):
+def read_score_data(year, month):
+    filename = f'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{year:04d}-{month:02d}.parquet'
+
     df = pd.read_parquet(filename)
     
     df['duration'] = df.tpep_dropoff_datetime - df.tpep_pickup_datetime
@@ -42,68 +21,26 @@ def read_data(filename):
     df = df[(df.duration >= 1) & (df.duration <= 60)].copy()
 
     df[categorical] = df[categorical].fillna(-1).astype('int').astype('str')
+
+    dicts = df[categorical].to_dict(orient='records')
+    X_val = dv.transform(dicts)
+    y_pred = model.predict(X_val)
     
-    return df
+    return y_pred
+
+#df = read_data('https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2023-03.parquet')
 
 
-# In[7]:
+def run():
+    year = int(sys.argv[1]) # 2021
+    month = int(sys.argv[2]) # 3
+    predictions = read_score_data(year,month)
+    print('rediction_mean: ', np.mean(predictions))
 
 
-df = read_data('https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2023-03.parquet')
+if __name__ == '__main__':
+    run()
 
-
-# In[8]:
-
-
-dicts = df[categorical].to_dict(orient='records')
-X_val = dv.transform(dicts)
-y_pred = model.predict(X_val)
-
-
-# In[10]:
-
-
-np.std(y_pred)
-
-
-# In[41]:
-
-
-df['predictions'] = y_pred
-
-
-# In[21]:
-
-
-df['year'] = df['tpep_pickup_datetime'].dt.strftime('%Y')
-df['month'] = df['tpep_pickup_datetime'].dt.strftime('%m')
-
-
-# In[38]:
-
-
-df['ride_id'] = df["year"].astype(str) + '/' + df["month"].astype(str) + '_' + df.index.astype(str)
-
-
-# In[43]:
-
-
-df_result = df[['ride_id','predictions']]
-
-
-# In[46]:
-
-
-output_file = './df_result.parquet'
-df_result.to_parquet(
-    output_file,
-    engine='pyarrow',
-    compression=None,
-    index=False
-)
-
-
-# In[ ]:
 
 
 
